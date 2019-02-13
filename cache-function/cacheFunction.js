44bef6e54ed0158508db08cc1e60ed3380da1d13
shivangg <xshivangx@gmail.com>
2019-02-14 02:04:08 +0530

    feat: Complete cache function

--- cache-function/cacheFunction.js
+++ cache-function/cacheFunction.js
@@ -1,6 +1,20 @@
 
-function cacheFunction(...args) {
-  return args;
+function cacheFunction(functionToBeCached) {
+  var cachedResults = {};
+
+  var theCacher = function (innerParams) {
+    if (cachedResults.hasOwnProperty(innerParams)) {
+      return cachedResults[innerParams];
+    } else {
+      cachedResults[innerParams] = functionToBeCached(innerParams);
+      // console.log('====================================');
+      // console.log("New cache value", cachedResults);
+      // console.log('====================================');
+      return cachedResults[innerParams];
+    }
+  }
+
+  return theCacher;
 }
 
 export {
