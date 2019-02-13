0ea1db33dc7eb50a7fdbe1a108ede53e7fd80f43
shivangg <xshivangx@gmail.com>
2019-02-14 00:40:53 +0530

    feat: Complete isTriangle

--- is-triangle/isTriangle.js
+++ is-triangle/isTriangle.js
@@ -1,6 +1,9 @@
 
-function isTriangle(...args) {
-  return args;
+function isTriangle(side1, side2, side3) {
+  var biggestSide = Math.max(side1, side2, side3);
+  var sumSmallerTwo = side1 + side2 + side3 - biggestSide;
+
+  return (sumSmallerTwo > biggestSide);
 }
 
 export {
