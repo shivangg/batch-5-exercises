816207be8f1b229a864341ecd6f82efc84076eba
shivangg <xshivangx@gmail.com>
2019-02-14 04:49:39 +0530

    feat: Complete largeOfFour

--- large-of-four/largeOfFour.js
+++ large-of-four/largeOfFour.js
@@ -1,6 +1,20 @@
 
-function largeOfFour(...args) {
-  return args;
+function largeOfFour(arrayOfNumberArrays) {
+  var largestNumberFromEachArray = [];
+
+  for (let index = 0; index < arrayOfNumberArrays.length; index++) {
+    var numberArray = arrayOfNumberArrays[index];
+    var largestNumber = numberArray[0];
+
+    for (let innerIndex = 0; innerIndex < numberArray.length; innerIndex++) {
+      var number = numberArray[innerIndex];
+      if (number > largestNumber) {
+        largestNumber = number;
+      }
+    }
+    largestNumberFromEachArray.push(largestNumber);
+  }
+  return largestNumberFromEachArray;
 }
 
 export {
