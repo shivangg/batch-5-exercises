3803817178a4f0b8d554f15af73330348f0218da
shivangg <xshivangx@gmail.com>
2019-02-14 04:32:32 +0530

    feat: Complete longestWordInString

--- longest-word-in-string/longestWordInString.js
+++ longest-word-in-string/longestWordInString.js
@@ -1,6 +1,19 @@
 
-function longestWordInString(...args) {
-  return args;
+function longestWordInString(string) {
+
+  var wordsArray = string.split(' ');
+  var longestWordLength = 0;
+
+  for (let index = 0; index < wordsArray.length; index++) {
+    var currentWordLength = wordsArray[index].length;
+    if (currentWordLength > longestWordLength ) {
+      longestWordLength = currentWordLength;
+    }
+
+  }
+
+
+  return longestWordLength;
 }
 
 export {
