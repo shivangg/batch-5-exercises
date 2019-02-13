acee83a428fc870e99e5d036cac98bbf84bee4ba
shivangg <xshivangx@gmail.com>
2019-02-14 04:12:30 +0530

    feat: Complete reverseString

--- reverse-string/reverseString.js
+++ reverse-string/reverseString.js
@@ -1,6 +1,11 @@
 
-function reverseString(...args) {
-  return args;
+function reverseString(string) {
+  var reverseString = "";
+  for (let index = 0; index < string.length; index++) {
+    var currentLetter = string[index];
+    reverseString = currentLetter + reverseString;
+  }
+  return reverseString;
 }
 
 export {
