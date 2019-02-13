2abe82c6a3f81007581dacf9cc03a284023ac5ba
shivangg <xshivangx@gmail.com>
2019-02-14 04:44:00 +0530

    feat: Complete sumAll

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,6 +1,20 @@
 
-function sumAll(...args) {
-  return args;
+function sumAll(numberPointsArray) {
+  var startNumber;
+  var endNumber;
+  if (numberPointsArray[0] < numberPointsArray[1]) {
+    startNumber = numberPointsArray[0];
+    endNumber = numberPointsArray[1];
+  } else {
+    startNumber = numberPointsArray[1];
+    endNumber = numberPointsArray[0];
+  }
+
+  var sumFromUnity = function (positiveNumber) {
+    return (positiveNumber * (positiveNumber + 1))/ 2;
+  }
+
+  return sumFromUnity(endNumber) - sumFromUnity(startNumber) + startNumber;
 }
 
 export {
