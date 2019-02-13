b736cebac4f748dcae36075907bf71a0636487de
shivangg <xshivangx@gmail.com>
2019-02-14 04:54:07 +0530

    feat: Complete repeatStringNumTimes

--- repeat-string-num-times/repeatStringNumTimes.js
+++ repeat-string-num-times/repeatStringNumTimes.js
@@ -1,6 +1,16 @@
 
-function repeatStringNumTimes(...args) {
-  return args;
+function repeatStringNumTimes(string, freq) {
+  var repeatedString = "";
+
+  if (freq < 0) {
+    return ""
+  }
+
+  for (let index = 0; index < freq; index++) {
+    repeatedString += string;
+  }
+
+  return repeatedString;
 }
 
 export {
