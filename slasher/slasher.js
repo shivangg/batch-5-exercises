5b15980ad22c501c4a025b2de2ad40464021522a
shivangg <xshivangx@gmail.com>
2019-02-14 23:58:04 +0530

    implement slasher

--- slasher/slasher.js
+++ slasher/slasher.js
@@ -1,6 +1,10 @@
 
-function slasher(...args) {
-  return args;
+function slasher(unSlashedValues, numberOfChops) {
+  var chopTillIndex = numberOfChops - 1;
+  var slashedValues = unSlashedValues.filter( ( el, index ) => {
+    return index > chopTillIndex;
+  } )
+  return slashedValues;
 }
 
 export {
