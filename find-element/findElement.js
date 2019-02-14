89220952abf2f06ebb376ba169b2d4a2920e50ba
shivangg <xshivangx@gmail.com>
2019-02-15 00:08:49 +0530

    implement findElement

--- find-element/findElement.js
+++ find-element/findElement.js
@@ -1,6 +1,9 @@
 
-function findElement(...args) {
-  return args;
+function findElement(numbers, truthtest) {
+  var satisfyingNumbers = numbers.filter( number => {
+    return truthtest(number);
+  } )
+  return satisfyingNumbers[0];
 }
 
 export {
