30fa33e32774476faf0229f308659e2e648ae2bb
shivangg <xshivangx@gmail.com>
2019-02-15 23:38:27 +0530

    wip

--- memoize/memoize.js
+++ memoize/memoize.js
@@ -1,6 +1,18 @@
 
-function memoize(...args) {
-  return args;
+function memoize(fn) {
+  var cachedResults = {};
+  function applyMemoization(...args) {
+    if (args in Object.keys(cachedResults)) {
+      return cachedResults[args];
+    }
+    cachedResults[args] = fn(args);
+    console.log('====================================');
+    console.log(cachedResults);
+    console.log('====================================');
+    return cachedResults[args];
+  }
+
+  return applyMemoization;
 }
 
 export {
--- savings-account/SavingsAccount.js
+++ savings-account/SavingsAccount.js
@@ -1,6 +1,17 @@
 
-function savingsAccount(...args) {
-  return args;
+function SavingsAccount(...args) {
+  const myAccount = {
+    _accountNumber: args[0],
+    _email: args[1],
+    _firstName: args[2],
+    _lastName: args[3],
+    products: [],
+  }
+
+  function
+
+
+  return myAccount;
 }
 
 export {
