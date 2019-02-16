c72df9ac0752194c8cc234d654d9148e116167b0
shivangg <xshivangx@gmail.com>
2019-02-17 05:09:21 +0530

    complete sumPrimes

--- sum-primes/sumPrimes.js
+++ sum-primes/sumPrimes.js
@@ -1,6 +1,21 @@
+function isPrime(number) {
+  const totalDivisors = new Array(number)
+    .fill(0)
+    .reduce(
+      (acc, currentValue, currentIndex) => {
+        const isDivisible = (number % (currentIndex + 1) === 0);
+        return isDivisible ? acc + 1 : acc;
+      }, 0,
+    );
+  return totalDivisors === 2;
+}
 
-function sumPrimes(...args) {
-  return args;
+function sumPrimes(n) {
+  const result = new Array(n + 1).fill().reduce((runningSum, currentValue, currentIndex) => {
+    const newSum = isPrime(currentIndex) ? runningSum + currentIndex : runningSum;
+    return newSum;
+  }, 0);
+  return result;
 }
 
 export {
