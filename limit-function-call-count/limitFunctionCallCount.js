f2ea9eccce444634e8206154b83d4c42f3bd80fa
shivangg <xshivangx@gmail.com>
2019-02-17 01:48:04 +0530

    complete limitFunctionCall

--- limit-function-call-count/limitFunctionCallCount.js
+++ limit-function-call-count/limitFunctionCallCount.js
@@ -1,6 +1,13 @@
 
-function limitFunctionCallCount(...args) {
-  return args;
+function limitFunctionCallCount(cb, callLimit) {
+  let numberOfCalls = 0;
+  function limitedCaller(...args) {
+    numberOfCalls += 1;
+    const callInLimit = numberOfCalls <= callLimit;
+    return (callInLimit) ? cb(...args) : null;
+  }
+
+  return limitedCaller;
 }
 
 export {
