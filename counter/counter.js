558f4e7f13294c8995744f621835e2406fe98993
shivangg <xshivangx@gmail.com>
2019-02-17 01:27:50 +0530

    complete counter

--- counter/counter.js
+++ counter/counter.js
@@ -1,6 +1,13 @@
 
-function counter(...args) {
-  return args;
+function counter() {
+  let numberOfTimesCalled = 0;
+
+  function keepCount() {
+    numberOfTimesCalled += 1;
+    return numberOfTimesCalled;
+  }
+
+  return keepCount;
 }
 
 export {
