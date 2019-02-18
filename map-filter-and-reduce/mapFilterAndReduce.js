b3920b6347e0e65144d096754d107f4c35157f62
shivangg <xshivangx@gmail.com>
2019-02-19 00:39:27 +0530

    complete map, filter, reduce

--- map-filter-and-reduce/mapFilterAndReduce.js
+++ map-filter-and-reduce/mapFilterAndReduce.js
@@ -1,6 +1,13 @@
+function mapFilterAndReduce(objectList) {
+  const objectWithLengthLessThan5 = objectList.filter(el => el.firstName.length < 5);
 
-function mapFilterAndReduce(...args) {
-  return args;
+  const nameWithLength = objectWithLengthLessThan5.reduce((acc, currentValue) => {
+    const updatedAcc = acc;
+    updatedAcc[currentValue.firstName] = currentValue.firstName.length;
+    return updatedAcc;
+  }, {});
+
+  return nameWithLength;
 }
 
 export {
