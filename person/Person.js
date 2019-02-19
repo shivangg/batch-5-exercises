22940bb256d5dafa53407978313d259729d6428c
shivangg <xshivangx@gmail.com>
2019-02-19 09:58:29 +0530

    complete person.js

--- person/Person.js
+++ person/Person.js
@@ -1,8 +1,21 @@
+class Person {
+  constructor(firstname, lastname, DOB) {
+    this.firstName = firstname;
+    this.lastName = lastname;
+    this.dateOfBirth = DOB;
+  }
 
-function person(...args) {
-  return args;
+  addDobDigits() {
+    const dateDigitsSum = this.dateOfBirth
+      .split('')
+      .filter(el => el !== '/')
+      .reduce((acc, el) => acc + parseInt(el, 10), 0);
+
+    // console.log(parseInt(day, 10) + parseInt(month, 10) + parseInt(year, 10));
+    return dateDigitsSum;
+  }
 }
 
 export {
-  person,
+  Person,
 };
