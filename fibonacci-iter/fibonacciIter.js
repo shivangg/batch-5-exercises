e84bafe3d7ef59b050cb45025ff74887a88d7341
shivangg <xshivangx@gmail.com>
2019-02-20 02:04:03 +0530

    complete fibonacciIter

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,7 +1,16 @@
-
-function fibonacciIter(...args) {
-  return args;
-}
+const fibonacciIter = {
+  [Symbol.iterator]: function* fibonacciIter() {
+    let prev = 0;
+    let next = 1;
+    let sum;
+    while (true) {
+      sum = prev + next;
+      yield sum;
+      prev = next;
+      next = sum;
+    }
+  },
+};
 
 export {
   fibonacciIter,
