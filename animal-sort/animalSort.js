9d9c8dc57d48b25c3943e5fcdfadb349876cf6cb
shivangg <xshivangx@gmail.com>
2019-02-20 05:19:40 +0530

    complete animalSort

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,6 +1,12 @@
-
-function animalSort(...args) {
-  return args;
+function animalSort(animals) {
+  return animals.sort((a, b) => {
+    const legDiff = a.numberOfLegs - b.numberOfLegs;
+    if (legDiff === 0) {
+      const stringDiff = a.name.charCodeAt() - b.name.charCodeAt();
+      return stringDiff;
+    }
+    return legDiff;
+  });
 }
 
 export {
