2d2a31bf30c987558b45cdd1ce080fd0f9413e1b
shivangg <xshivangx@gmail.com>
2019-02-19 10:51:07 +0530

    completed sequential promise

--- sequential-promise/sequentialPromise.js
+++ sequential-promise/sequentialPromise.js
@@ -1,6 +1,10 @@
-
-function sequentialPromise(...args) {
-  return args;
+function sequentialPromise([promise1, promise2, promise3]) {
+  const twoPromiseResolvation = promise1()
+    .then(res => promise2(res));
+  if (promise3 !== undefined) {
+    return twoPromiseResolvation.then(res => promise3(res));
+  }
+  return twoPromiseResolvation;
 }
 
 export {
