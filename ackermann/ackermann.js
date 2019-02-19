f10fa0d730eb1226d8b9f8892e35a5062f50a0ba
shivangg <xshivangx@gmail.com>
2019-02-20 04:49:07 +0530

    complete ackerman

--- ackermann/ackermann.js
+++ ackermann/ackermann.js
@@ -1,6 +1,18 @@
+const obj = (m, n) => ({ m, n });
 
-function ackermann(...args) {
-  return args;
+function ackermann({ m, n }) {
+  if (m === 0) {
+    return n + 1;
+  }
+
+  if (m > 0) {
+    if (n === 0) {
+      return ackermann(obj(m - 1, 1));
+    }
+    const result = obj(m - 1, ackermann(obj(m, n - 1)));
+    return ackermann(result);
+  }
+  return true;
 }
 
 export {
