ccffdda9547106b173690127d072a65a3bb3c330
shivangg <xshivangx@gmail.com>
2019-02-19 11:14:52 +0530

    complete alphabetShift

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,7 @@
-
-function alphabeticShift(...args) {
-  return args;
+function alphabeticShift(string) {
+  return string.split('')
+    .map(char => String.fromCharCode(char.charCodeAt(0) + 1))
+    .join('');
 }
 
 export {
