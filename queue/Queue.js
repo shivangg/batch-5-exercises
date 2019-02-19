646eeef2e4d3b761697e3df9f832105062902934
shivangg <xshivangx@gmail.com>
2019-02-19 11:35:42 +0530

    complete queue

--- queue/Queue.js
+++ queue/Queue.js
@@ -1,8 +1,39 @@
+class Queue {
+  constructor() {
+    this.data = [];
+  }
 
-function queue(...args) {
-  return args;
+  enqueue(element) {
+    this.data = [...this.data, element];
+  }
+
+  dequeue() {
+    const top = this.data[0] || null;
+    this.data = this.data.filter((value, index) => index !== 0);
+    return top;
+  }
+
+  isEmpty() {
+    return this.data.length === 0;
+  }
+
+  toString(fn) {
+    let stringData = this.data;
+    if (fn) {
+      stringData = this.data.map(fn);
+    }
+    return stringData.join(',');
+  }
+
+  peek() {
+    const queueLength = this.data.length;
+    if (queueLength === 0) {
+      return null;
+    }
+    return this.data[0];
+  }
 }
 
 export {
-  queue,
+  Queue,
 };
