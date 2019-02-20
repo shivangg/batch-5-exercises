78b3c029f6fc7fd4c981474998e091dac5620917
shivangg <xshivangx@gmail.com>
2019-02-21 04:58:46 +0530

    complete slice

--- slice/slice.js
+++ slice/slice.js
@@ -1,6 +1,8 @@
-
-function slice(...args) {
-  return args;
+function slice(fromIndex, toIndex, values) {
+  if (typeof values === 'string') {
+    return values.split('').slice(fromIndex, toIndex).join('');
+  }
+  return values.slice(fromIndex, toIndex);
 }
 
 export {
