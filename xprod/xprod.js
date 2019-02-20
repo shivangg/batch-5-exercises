c4018df3393bb81da94701753d8a1f585ea5bf73
shivangg <xshivangx@gmail.com>
2019-02-21 01:40:00 +0530

    complete xProd

--- xprod/xprod.js
+++ xprod/xprod.js
@@ -1,6 +1,23 @@
+const isEmpty = list => list.length === 0;
 
-function xprod(...args) {
-  return args;
+const someEmpty = (...lists) => lists.reduce((acc, list) => acc || isEmpty(list), false);
+
+function xprod(list1, list2) {
+  if (someEmpty(list1, list2)) {
+    return [];
+  }
+
+  const combinations = list1.reduce((acc, currentValue) => {
+    const newElement = list2
+      .reduce((innerAcc, currentBiggerValue) => [...innerAcc, [currentValue, currentBiggerValue]],
+        []);
+    const newAcc = [
+      ...acc,
+      ...newElement,
+    ];
+    return newAcc;
+  }, []);
+  return combinations;
 }
 
 export {
