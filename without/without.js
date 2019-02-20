8f57d454eb86842c2588bd43d38d12ccbf9fdb5e
shivangg <xshivangx@gmail.com>
2019-02-21 00:16:39 +0530

    complete without exercise

--- without/without.js
+++ without/without.js
@@ -1,6 +1,10 @@
+const isMinusZero = number => (1 / number) === -Infinity;
+const stringifyValues = list => list.map(el => (isMinusZero(el) ? '-0' : el.toString()));
 
-function without(...args) {
-  return args;
+function without(numberTobeRemoved, numbers) {
+  const numbersWithout = stringifyValues(numbers)
+    .filter(el => !stringifyValues(numberTobeRemoved).includes(el));
+  return numbersWithout.map(el => Number(el));
 }
 
 export {
