683bfa1cbf6d58840b3d08cdb7a6e647ae1cdd57
shivangg <xshivangx@gmail.com>
2019-02-22 22:10:03 +0530

    complete sleepPromise

--- sleep-promise/sleepPromise.js
+++ sleep-promise/sleepPromise.js
@@ -1,8 +1,15 @@
+function sleep(ms) {
+  const p = new Promise((res) => {
+    setTimeout(res, ms);
+  });
+  const innerFn = value => p.then(() => value);
 
-function sleepPromise(...args) {
-  return args;
+  innerFn.then = (...args) => p.then(...args);
+  innerFn.catch = Promise.resolve().catch;
+
+  return innerFn;
 }
 
 export {
-  sleepPromise,
+  sleep,
 };
