b868346c459c1f5444a1499b90ac403f8faca903
shivangg <xshivangx@gmail.com>
2019-02-24 06:40:56 +0530

    complete palindrome exercise

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,6 +1,27 @@
+function sanitizeString(string) {
+  const onlyAlphaNumericString = Array.from(string)
+    .filter((el) => {
+      const alphaNumericRegex = /[a-zA-Z0-9]/;
+      return alphaNumericRegex.test(el);
+    })
+    .map((onlyAlphanumericChar) => {
+      if (/[A-Z]/.test(onlyAlphanumericChar)) {
+        return onlyAlphanumericChar.toLowerCase();
+      }
+      return onlyAlphanumericChar;
+    })
+    .join('');
+  return onlyAlphaNumericString;
+}
 
-function isPalindrome(...args) {
-  return args;
+function reverse(string) {
+  return string.split('').reverse().join('');
+}
+
+function isPalindrome(string) {
+  const sanitizedString = sanitizeString(string.toString());
+  const isEqualToreverse = reverse(sanitizedString) === sanitizedString;
+  return isEqualToreverse;
 }
 
 export {
