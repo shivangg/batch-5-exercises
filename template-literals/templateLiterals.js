a8123865af3f84025e09e31d8d6c330c54a21fd1
shivangg <xshivangx@gmail.com>
2019-02-25 02:21:59 +0530

    complete template literal

--- template-literals/templateLiterals.js
+++ template-literals/templateLiterals.js
@@ -1,6 +1,5 @@
-
-function templateLiterals(...args) {
-  return args;
+function templateLiterals(players = 4) {
+  return `There are ${players} people on the football team. Their names are Rooney, Ronaldo, Messi, Pogba.`;
 }
 
 export {
