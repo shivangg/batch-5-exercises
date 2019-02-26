331c8239516b7f391430e4c1581d6d39509cb1ff
shivangg <xshivangx@gmail.com>
2019-02-27 00:53:44 +0530

    complete movieList

--- beginner-exercises/src/components/02-MovieList.jsx
+++ beginner-exercises/src/components/02-MovieList.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 // We will create dynamic list of movies. Let's see how to render
 // a list of items.
@@ -6,19 +6,19 @@ import React, { Component } from 'react';
 // Task #1: Add another movie (or movies) to the list.
 // Task #2: Change `ul` HTML attribute `class` to 'movies-list'.
 
-class MovieList extends Component {
+function MovieList() {
   // We can put DOM elements delivered by React just like HTML elements.
   // Doesn't this code look familiar to you?
   // Refer to App.css for a list a classes or add one yourself
-  render() {
-    return (
-      <ul className="FILL ME">
-        <li>The Godfather</li>
-      </ul>
-    );
-  }
+  return (
+    <ul className="movie-list" >
+      <li> The Godfather </li>
+      <li> The Godfather Part II </li>
+    </ul>
+  );
   // Think about it: Why is that `className` attribute not called just `class`?
   // Hint: Some words in JavaScript/JSX are reserved...
+  // Ans: The word class has a special meaning in JS
 }
 
 export default MovieList;
