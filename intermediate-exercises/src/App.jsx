41ae10b08fe7aed2b32d9462d6c65d3165f941dd
shivangg <xshivangx@gmail.com>
2019-02-27 08:40:47 +0530

    fix eslint

--- intermediate-exercises/src/App.jsx
+++ intermediate-exercises/src/App.jsx
@@ -35,6 +35,7 @@ const App = () => (
       <hr />
       {routes.map(route => (
         <Route
+          key={route.path}
           path={route.path}
           render={props => (
             <route.component {...props} routes={route.routes} />
