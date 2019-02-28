cfce872e2361e210e9edd3a895cedf0f4805a135
shivangg <xshivangx@gmail.com>
2019-03-01 00:56:47 +0530

    Complete router exercise

--- setup-router/src/App.js
+++ setup-router/src/App.js
@@ -1,20 +1,14 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import Navbar from "./components/Navbar";
 
-import './styles/App.css';
+import "./styles/App.css";
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Navbar />
-        <Home />
-        <About />
-        <Contact />
       </Fragment>
     );
   }
---
+++ setup-router/src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch
+} from "react-router-dom";
+
+import "../styles/App.css";
+
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Error404 from "./Error404";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul className="nav">
+              <li>
+                <NavLink to="/home">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">Contact</NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/home" component={Home} />
+            <Route path="/about/" component={About} />
+            <Route path="/contact/" component={Contact} />
+            <Route component={Error404} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
