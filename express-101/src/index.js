2b9803a7479e73ec493f699ba5ea0c6bc2367edf
shivangg <xshivangx@gmail.com>
2019-03-07 00:27:12 +0530

    complete express 101

--- express-101/src/index.js
+++ express-101/src/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 
 const app = express();
+const PORT = 3000;
 
 // 1) Create Routes
 
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+app.get('/movie/:title', (req, res) => {
+  res.send(`My favorite movie is ${req.params.title}`);
+});
+
 // 2) Start server on port 3000
+app.listen(PORT, () => console.log(`Server listening on port:${PORT}`));
 
 module.exports = app;
