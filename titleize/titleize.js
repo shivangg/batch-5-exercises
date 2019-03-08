a1c5e6a92b3142908e02c33ec757dfb1a3812908
shivangg <xshivangx@gmail.com>
2019-03-09 00:53:07 +0530

    complete titleize

--- titleize/titleize.js
+++ titleize/titleize.js
@@ -1,8 +1,26 @@
+function titleize(sentence) {
+  if (sentence === '') {
+    return '';
+  }
+  const isFirstElement = index => index === 0;
+  const isElementASymbol = (char, symbolList) => symbolList.some(symbol => symbol === char);
+  const words = sentence.split(' ');
+  const space = ' ';
 
-function titleize(...args) {
-  return args;
+  return words
+    .map(word => word.split('').reduce((acc, char, index) => {
+      if (char === ' ') {
+        return `${acc}${space}`;
+      }
+      if (isFirstElement(index)) {
+        return `${acc}${char.toUpperCase()}`;
+      }
+      if (isElementASymbol(acc[acc.length - 1], ['-'])) {
+        return `${acc}${char.toUpperCase()}`;
+      }
+      return `${acc}${char.toLowerCase()}`;
+    }, ''))
+    .join(' ');
 }
 
-export {
-  titleize,
-};
+export { titleize };
